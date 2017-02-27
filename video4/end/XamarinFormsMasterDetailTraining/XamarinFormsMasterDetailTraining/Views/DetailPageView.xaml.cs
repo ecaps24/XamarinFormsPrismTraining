294f4c0894a17d7a004f5a59d625e19646e258c7
Aaron LaBeau <alabeau@outlook.com>
2017-02-26 23:47:57 -0500

    Video setup

---
+++ video4/end/XamarinFormsMasterDetailTraining/XamarinFormsMasterDetailTraining/Views/DetailPageView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsMasterDetailTraining.Views
+{
+	public partial class DetailPageView : ContentPage
+	{
+		public DetailPageView()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
